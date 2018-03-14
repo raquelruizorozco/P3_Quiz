ed74376fadef817c5186e5b1a03c645ae3e7690b
raquelruizorozco <36446754+raquelruizorozco@users.noreply.github.com>
2018-03-14 12:21:18 +0100

    Add files via upload

--- cmds.js
+++ cmds.js
@@ -1,8 +1,7 @@
 /**
  * Created by raquel.ruiz.orozco on 28/02/18.
  */
-const Sequelize = require('sequelize');
-
+const Sequelize = require('sequelize'); //cargamos modulo sequelize
 const { log, biglog, errorlog, colorize} = require("./out");
 const {models} = require('./model');
 
@@ -28,7 +27,7 @@ exports.listCmd = rl => {
     rl.prompt();
     */
 
-    models.quiz.findAll() //Devuelve un array con los quizzes existentes
+    models.quiz.findAll() //te da los quizzes existentes
         .each(quiz => { // Coge todos los quiz que hay dentro del array que devuelve findAll
         log(` [${colorize(quiz.id, 'magenta')}]: ${quiz.question}`);
 })
@@ -40,13 +39,14 @@ exports.listCmd = rl => {
 })
 
 };
-const validateId = id => {
 
-    return new Sequelize.Promise((resolve, reject) => {// Sequilize.Promise - promesas de sequielize
+const validateId = id => {
+//funcion validacion: voy a querer que me devuelva id
+    return new Sequelize.Promise((resolve, reject) => {
         if (typeof id === "undefined") {
         reject(new Error(`Falta el parametro <id>.`));
     } else {
-        id = parseInt(id); //coger la parte entera y descartar lo demás
+        id = parseInt(id); //coger la parte entera y descartar lo demas
         if (Number.isNaN(id)) {
             reject(new Error(`El valor del parámetro <id> no es un número`))
         } else {
@@ -58,12 +58,15 @@ const validateId = id => {
 
 exports.showCmd = (rl, id) => {
 
-    validateId(id)
-        .then(id => models.quiz.findById(id)) //Del modelo de datos voy al modelo quiz y busco un quiz por id
+    validateId(id) // me devuelve una promesa
+        //en mi modelo de dato voy quiz y lo busco por id
+        .then(id => models.quiz.findById(id))
 .then(quiz => {
+    //compruebo si realmente me ha pasado un quiz
         if (!quiz) {
         throw new Error(`No existe un quiz asociado al id=${id}.`);
     }
+
     log(` [${colorize(quiz.id, 'magenta')}]: ${quiz.question} ${colorize('=>', 'magenta')} ${quiz.answer}`);
 })
 .catch(error => {
@@ -85,20 +88,21 @@ const makeQuestion = (rl, text) => {
 
 exports.addCmd = rl => {
 
-    makeQuestion(rl, 'Introduzca una pregunta: ') //Hasta que no introduzca una pregunta no finaliza
+    makeQuestion(rl, 'Introduzca una pregunta: ') //promesa que hasta no introduzca una pregunta no finaliza
         .then(q => {
         return makeQuestion(rl, 'Introduce la respuesta: ')
             .then(a => {
-            return {question: q, answer: a};
+                //creo una pregunta para introducir el texto de la respuesta
+            return {question: q, answer: a}; //creo un objeto quiz
 });
 })
 .then(quiz => {
-        return models.quiz.create(quiz);
+        return models.quiz.create(quiz); //creo objeto en tabla de base de datos
 })
 .then((quiz) => {
         log(` ${colorize('Se ha añadido', 'magenta')}: ${quiz.question} ${colorize('=>', 'magenta')} ${quiz.answer}`);
 })
-.catch(Sequelize.ValidationError, error => { //Si hay errores de validación
+.catch(Sequelize.ValidationError, error => { //error de validacion
         errorlog('El quiz es erroneo: ');
     error.errors.forEach(({message}) => errorlog(message));
 })
@@ -113,7 +117,7 @@ exports.addCmd = rl => {
 exports.deleteCmd = (rl, id) => {
 
     validateId(id)
-        .then(id => models.quiz.destroy({where: {id}})) //Condición: el elemento que quiero destruir es el que tiene como id el valor id
+        .then(id => models.quiz.destroy({where: {id}})) //el elemento que quiero destruir es el que tiene como id el valor id
 .catch(error => {
         errorlog(error.message);
 })
@@ -130,11 +134,11 @@ exports.editCmd = (rl,id) => {
         throw new Error(`No existe el parametro asociado ${id}.`);
     }
 
-    process.stdout.isTTY && setTimeout(() => {rl.write(quiz.question)},0);
-    return makeQuestion(rl, ' Introduzca la pregunta: ')
+    process.stdout.isTTY && setTimeout(() => {rl.write(quiz.question)},0); //busco la pregunta que tengo que editar por id
+    return makeQuestion(rl, ' Introduzca la pregunta: ') //edito pregunta
         .then(q => {
         process.stdout.isTTY && setTimeout(() => {rl.write(quiz.answer)},0);
-    return makeQuestion(rl, 'Introduzca la respuesta ')
+    return makeQuestion(rl, 'Introduzca la respuesta ') //edito respuesta
         .then(a => {
         quiz.question =q;
     quiz.answer =a;
@@ -148,7 +152,7 @@ exports.editCmd = (rl,id) => {
 .then(quiz => {
         log (`Se ha cambiado el quiz ${colorize(quiz.id, 'magenta')} por: ${quiz.question} ${colorize('=>', 'magenta')} ${quiz.answer}`)
 })
-.catch(Sequelize.ValidationError, error => {
+.catch(Sequelize.ValidationError, error => { //solo para errores de validacion
         errorlog('El quiz es erroneo:');
     error.errors.forEach(({message}) => errorlog(message));
 })
